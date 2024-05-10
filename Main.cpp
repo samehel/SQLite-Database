e51cea62634b329ecf8b276067568d154757c1cd
Sameh <sameh.elwakeel75@gmail.com>
2024-05-10 21:26:17 +0300

    Added better error handling to main function

--- Main.cpp
+++ Main.cpp
@@ -16,19 +16,26 @@
  * executes the corresponding function for each command,
  * and displays an error message if an invalid command is entered.
  */
-void main() {
+int main() {
 	InputBuffer inputBuffer;
 	while(true) {
 
 		inputBuffer.read_input();
 		map<string, void(*)()> commands = {
-			{ "exit", ExitDatabase },
+			{ ".exit", ExitDatabase },
 		};
 
 		auto execCmd = commands.find(inputBuffer.returnCmd());
 		if (execCmd != commands.end())
 			execCmd->second();
-		else
-			cout << "ERR: The command you are trying to execute does not exist.\n";
+		else {
+			if (inputBuffer.returnCmd() == "" || inputBuffer.returnCmd() == ". " || inputBuffer.returnCmd().find(' ') != string::npos)
+				cout << "Your command cannot contain empty spaces.\n";
+			else if (inputBuffer.returnCmd()[0] != '.')
+				cout << "Did you try " + string(PREFIX) + inputBuffer.returnCmd() + "?" << endl;
+			else
+				cout << "ERR: The command you are trying to execute does not exist.\n";
+		}
+
 	}
 }
